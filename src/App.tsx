82affb0c28094a089b9d48906dd2616db952807a
HimanshuShukla012 <himanshushukla943@gmail.com>
2026-09-25 10:11:23 +0530

    link remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import PCIGISDashboard from "./PCIGISDashboard";
 import SwachhBharatVideoHub from "./videoscreen"
 
